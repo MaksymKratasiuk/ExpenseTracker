5975466be9723afc19c9b05b9c088ca46638ddee
MaksymKratasiuk <makshm1307@gmail.com>
2024-06-25 12:46:46 +0300

    Налаштований роутинг

--- teamProject/Program.cs
+++ teamProject/Program.cs
@@ -8,49 +8,6 @@ namespace teamProject
     {
         public static void Main(string[] args)
         {
-            //var builder = WebApplication.CreateBuilder(args);
-
-            //// Add services to the container.
-            //var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            //builder.Services.AddDbContext<ApplicationDbContext>(options =>
-            //    options.UseSqlite(connectionString));
-            //builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-            //builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-            //    .AddEntityFrameworkStores<ApplicationDbContext>();
-            //builder.Services.AddRazorPages();
-
-            //var app = builder.Build();
-
-            //// Configure the HTTP request pipeline.
-            //if (app.Environment.IsDevelopment())
-            //{
-            //    app.UseMigrationsEndPoint();
-            //}
-            //else
-            //{
-            //    app.UseExceptionHandler("/Error");
-            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            //    app.UseHsts();
-            //}
-
-            //app.UseHttpsRedirection();
-            //app.UseStaticFiles();
-
-            //app.UseRouting();
-
-            //app.UseAuthorization();
-
-            //app.MapRazorPages();
-
-            //app.UseEndpoints(endpoints =>
-            //{
-            //    endpoints.MapRazorPages();
-            //    endpoints.MapFallbackToPage("/Dashboard/Index");
-            //});
-
-            //app.Run();
-
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
@@ -73,7 +30,6 @@ namespace teamProject
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
@@ -87,6 +43,14 @@ namespace teamProject
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+
+                // Add this line to redirect the root URL to /Dashboard/Index
+                endpoints.MapGet("/", context =>
+                {
+                    context.Response.Redirect("/Dashboard/Index");
+                    return System.Threading.Tasks.Task.CompletedTask;
+                });
+
                 endpoints.MapFallbackToPage("/Dashboard/Index");
             });
 
