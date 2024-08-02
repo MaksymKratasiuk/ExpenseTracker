d63a1fae63999365425b39280e6f6f827213c356
MaksymKratasiuk <makshm1307@gmail.com>
2024-08-02 15:31:34 +0300

    Була додана ліцензія для шаблонних елементів

--- teamProject/Program.cs
+++ teamProject/Program.cs
@@ -16,9 +16,14 @@ namespace teamProject
                 options.UseSqlite(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
+
+
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddRazorPages();
+
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mgo+DSMBMAY9C3t2U1hhQlJBfVddXmJWfFN0QXNYfVR0cF9GYkwxOX1dQl9nSXZTd0RhXHpccXJWRWY=;Mgo+DSMBPh8sVXJyS0d+X1RPfkBBXHxLflFyVWJbdVp0flVEcDwsT3RfQFljQX5Sd0dmXH1fdndWQA==");
+
 
             var app = builder.Build();
 
